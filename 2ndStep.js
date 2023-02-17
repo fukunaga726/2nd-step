82d38d372f89f19e8d422e3a1c5038c0a3eccb97
fukunaga627 <cod22tail@gmail.com>
2023-02-17 19:25:58 +0900

    get JSON

--- 2ndStep.js
+++ 2ndStep.js
@@ -1,1 +1,36 @@
+"use strict"
+
+let url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=b19bfb39974728801a922faab371fff3"
+
+fetch(url) 
+     .then(response => response.json())
+     .then(data => {
+        console.log(data);
+     });
+
+     
+
+
+    // 東京のデータは？　上のurlで東京に絞り込み済み
+    // 気温は？
+    // 降水確率は？
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
