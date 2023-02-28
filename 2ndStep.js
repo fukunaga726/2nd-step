dd3d02e90f79549c8a09823a9e665a48177aaf79
fukunaga627 <cod22tail@gmail.com>
2023-02-28 09:57:41 +0900

    display data

--- 2ndStep.js
+++ 2ndStep.js
@@ -2,35 +2,27 @@
 
 let url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=b19bfb39974728801a922faab371fff3"
 
+const temp = document.getElementById("temp");
+const cloud = document.getElementById("cloud");
+const icon = document.getElementById("icon");
+
 fetch(url) 
      .then(response => response.json())
      .then(data => {
-        console.log(data);
-     });
+         console.log(data);
+         
 
-     
+         const dataTemp = (data.main.temp) - 273.15;
+         const dataCloud = data.clouds.all;
+         const dataIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+         
+         temp.innerHTML = dataTemp.toFixed(2);
+         cloud.innerHTML = dataCloud;
+         icon.innerHTML = dataIcon;
 
-
-    // 東京のデータは？　上のurlで東京に絞り込み済み
-    // 気温は？
-    // 降水確率は？
-
-
-
-
-
-
+        
+        });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+        
+    //  アイコンのURL : http://openweathermap.org/img/w/04n.png
