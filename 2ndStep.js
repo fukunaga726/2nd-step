417b509cc45e317d4851d2ca6ce99913990cf4ae
fukunaga627 <cod22tail@gmail.com>
2023-03-22 14:15:03 +0900

    set .gitignore

--- 2ndStep.js
+++ 2ndStep.js
@@ -1,10 +1,15 @@
 "use strict"
 
-let url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=b19bfb39974728801a922faab371fff3"
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${API_KEY}`
+
+
 
 const temp = document.getElementById("temp");
 const cloud = document.getElementById("cloud");
 const icon = document.getElementById("icon");
+
+
+
 
 fetch(url) 
      .then(response => response.json())
@@ -14,15 +19,15 @@ fetch(url)
 
          const dataTemp = (data.main.temp) - 273.15;
          const dataCloud = data.clouds.all;
-         const dataIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+         const tempIcon = "";
          
-         temp.innerHTML = dataTemp.toFixed(2);
-         cloud.innerHTML = dataCloud;
-         icon.innerHTML = dataIcon;
+         temp.innerHTML = `気温:${dataTemp.toFixed(2)}℃`;
+         cloud.innerHTML = `降水確率:${dataCloud}%`;
 
+        iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+        icon.scr = iconUrl;
         
         });
 
 
         
-    //  アイコンのURL : http://openweathermap.org/img/w/04n.png
